88cbc5a6d3e1523378b38cc49cf1b270e8320ceb
matondojoao <matondojoaokitemoco@gmail.com>
2023-12-16 20:47:16 +0100

    Habilita a utilização de fila para notificações de e-mail

--- app/Notifications/ResetPasswordNotification.php
+++ app/Notifications/ResetPasswordNotification.php
@@ -8,7 +8,7 @@ use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 use Illuminate\Support\Facades\DB;
 
-class ResetPasswordNotification extends Notification
+class ResetPasswordNotification extends Notification implements ShouldQueue
 {
     use Queueable;
 
