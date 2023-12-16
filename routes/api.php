a98e6ace6dc540b9f0508412e7cd6bd9b95a5a1e
matondojoao <matondojoaokitemoco@gmail.com>
2023-12-16 20:34:08 +0100

    Desativar a verificação do certificado no envio de e-mails

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\V1\Auth\AuthController;
+use App\Http\Controllers\Api\V1\Auth\ResetPasswordController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\V1\Public\ProductController as PublicProductController;
