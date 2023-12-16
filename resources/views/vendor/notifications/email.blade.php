30952523d8d4321c0db14fd460e594f69f5e8bf2
matondojoao <matondojoaokitemoco@gmail.com>
2023-12-16 20:44:44 +0100

    Desativar a verificação do certificado no envio de e-mails

--- resources/views/vendor/notifications/email.blade.php
+++ resources/views/vendor/notifications/email.blade.php
@@ -6,7 +6,6 @@
 @if ($level === 'error')
 # @lang('Whoops!')
 @else
-# @lang('Hello!')
 @endif
 @endif
 
@@ -35,24 +34,17 @@
 
 @endforeach
 
-{{-- Salutation --}}
-@if (! empty($salutation))
-{{ $salutation }}
-@else
-@lang('Regards'),<br>
-{{ config('app.name') }}
-@endif
-
 {{-- Subcopy --}}
 @isset($actionText)
 <x-slot:subcopy>
-@lang(
-    "If you're having trouble clicking the \":actionText\" button, copy and paste the URL below\n".
-    'into your web browser:',
-    [
-        'actionText' => $actionText,
-    ]
-) <span class="break-all">[{{ $displayableActionUrl }}]({{ $actionUrl }})</span>
+    @lang(
+        "Se você estiver com dificuldades para clicar no botão \":actionText\", copie e cole o URL abaixo\n".
+        'no seu navegador da web:',
+        [
+            'actionText' => $actionText,
+        ]
+    )
+    <span class="break-all">[{{ $displayableActionUrl }}]({{ $actionUrl }})</span>
 </x-slot:subcopy>
 @endisset
 </x-mail::message>
