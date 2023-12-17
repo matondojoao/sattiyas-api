f2d26767f5a5ee9df325834a7ef229051b410e6a
matondojoao <matondojoaokitemoco@gmail.com>
2023-12-17 13:11:02 +0100

    Implementação da lógica de verificação de e-mail

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,13 +4,14 @@ namespace App\Models;
 
 use App\Models\Traits\UuidTrait;
 use App\Notifications\ResetPasswordNotification;
+use App\Notifications\sendEmailVerificationNotification;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmail
 {
     use HasApiTokens, HasFactory, Notifiable, UuidTrait;
 
@@ -107,4 +108,10 @@ class User extends Authenticatable
     {
         return $this->belongsTo(Review::class);
     }
+
+    public function sendEmailVerificationNotification()
+    {
+        $this->notify(new sendEmailVerificationNotification());
+    }
+
 }
---
+++ app/Notifications/sendEmailVerificationNotification.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class sendEmailVerificationNotification extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+            ->subject('Verifique seu endereço de e-mail')
+            ->line('Clique no botão abaixo para verificar seu endereço de e-mail.')
+            ->line('Obrigado por usar nosso aplicativo!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
