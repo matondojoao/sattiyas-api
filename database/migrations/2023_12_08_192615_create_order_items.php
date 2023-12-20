1aafc3d62f3d880dc84cb4d6716bdae983f39daf
matondojoao <matondojoaokitemoco@gmail.com>
2023-12-20 11:45:33 +0100

    Adicionar recurso de gerenciamento de stocks

--- database/migrations/2023_12_08_192615_create_order_items.php
+++ database/migrations/2023_12_08_192615_create_order_items.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->uuid('id')->primary();
             $table->uuid('order_id');
             $table->uuid('product_id');
-            $table->integer('quantity');
+            $table->decimal('quantity');
             $table->decimal('price', 10, 2);
 
             $table->foreign('order_id')->references('id')->on('orders')->onDelete('cascade');
