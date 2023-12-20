54fc8581fe0c97f032aad86accaadab0390e6903
matondojoao <matondojoaokitemoco@gmail.com>
2023-12-20 15:08:31 +0100

    Adicionando admin como role padrao no cadastro de usuarios

--- app/Http/Controllers/Api/V1/Auth/AuthController.php
+++ app/Http/Controllers/Api/V1/Auth/AuthController.php
@@ -41,7 +41,7 @@ class AuthController extends Controller
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
-            'role' => 'admin',
+            'role' => 'customer',
             'password' => Hash::make($request->password),
         ]);
 
