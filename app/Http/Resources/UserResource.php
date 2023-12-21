074f7ade2de92b5c79d92c3166bac6b0bab22267
matondojoao <matondojoaokitemoco@gmail.com>
2023-12-21 11:12:48 +0100

    Implementa upload e armazenamento de fotos de perfil

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -22,7 +22,7 @@ class UserResource extends JsonResource
             'phone' => $this->phone,
             'alternative_phone' => $this->alternative_phone,
             'gender' => $this->gender,
-            'photo_path' => url('storage/' . $this->photo_path),
+            'photo_path' =>$this->photo_path ? url('storage/' . $this->photo_path) : null,
             'shipping_address'=>new ShippingAddressResource($this->whenLoaded('shippingAddress')),
             'billing_address'=>new ShippingAddressResource($this->whenLoaded('billingAddress'))
         ];
