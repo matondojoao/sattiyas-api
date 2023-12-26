4a077dd9dcf4803236dc93e67fdbdb001214e886
matondojoao <matondojoaokitemoco@gmail.com>
2023-12-26 21:21:28 +0100

    Adiciona recurso de assinatura de newsletter com model, repositório, controller, request, migration e notificação

--- app/Models/Newsletter.php
+++ app/Models/Newsletter.php
@@ -2,11 +2,18 @@
 
 namespace App\Models;
 
+use App\Models\Traits\UuidTrait;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Notifications\Notifiable;
 
 class Newsletter extends Model
 {
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, UuidTrait;
+
+    public $incrementing = false;
+
+    protected $keyType = 'uuid';
+
+    protected $fillable = ['email'];
 }
