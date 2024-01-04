86a29fd3c4baa8e70106888db18c1b06ecdba227
matondojoao <matondojoaokitemoco@gmail.com>
2024-01-04 11:28:59 +0100

    Removendo auto incremento na model ProductSize

--- app/Models/ProductSize.php
+++ app/Models/ProductSize.php
@@ -2,17 +2,12 @@
 
 namespace App\Models;
 
-use App\Models\Traits\UuidTrait;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class ProductSize extends Model
 {
-    use HasFactory, UuidTrait;
+    use HasFactory;
 
     protected $table='product_size';
-
-    public $incrementing = false;
-
-    protected $keyType = 'uuid';
 }
