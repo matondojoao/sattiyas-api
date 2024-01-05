42f4498b33748c47840d1d50522b7a7e354b4e61
matondojoao <matondojoaokitemoco@gmail.com>
2024-01-05 15:30:20 +0100

    Actualizando configuracao de cors

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -15,6 +15,7 @@ class Kernel extends HttpKernel
      */
     protected $middleware = [
         // \App\Http\Middleware\TrustHosts::class,
+        \App\Http\Middleware\Cors::class,
         \Illuminate\Session\Middleware\StartSession::class,
         \App\Http\Middleware\TrustProxies::class,
         \Fruitcake\Cors\HandleCors::class,
