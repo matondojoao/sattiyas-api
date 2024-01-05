59f9ed7dd515ed44e9bac9d40ae11b87dcec272c
matondojoao <matondojoaokitemoco@gmail.com>
2024-01-05 15:15:15 +0100

    Actualizando configuracao de cors

--- config/cors.php
+++ config/cors.php
@@ -16,19 +16,12 @@ return [
     */
 
     'paths' => ['api/*', 'sanctum/csrf-cookie','/*','*'],
-
     'allowed_methods' => ['*'],
-
     'allowed_origins' => ['*','http://localhost:5173'],
-
     'allowed_origins_patterns' => [],
-
     'allowed_headers' => ['*'],
-
     'exposed_headers' => [],
-
     'max_age' => 0,
-
-    'supports_credentials' => true,
+    'supports_credentials' => false,
 
 ];
