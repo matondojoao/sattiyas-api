b1ab9bcce13589b8212d3d64dffad42fc39184a3
matondojoao <matondojoaokitemoco@gmail.com>
2024-01-06 10:32:46 +0100

    Corrujindo calculo do total no carrinho

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -46,9 +46,6 @@ class Kernel extends HttpKernel
             'throttle:api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             ],
-
-            \App\Http\Middleware\EncryptCookies::class,
-            \Illuminate\Session\Middleware\StartSession::class,
     ];
 
     /**
