1e796f9d8645ae1150afece4b192d940455a156f
matondojoao <matondojoaokitemoco@gmail.com>
2024-01-07 15:18:04 +0100

    Actualizando configuração de cors

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -45,6 +45,7 @@ class Kernel extends HttpKernel
             \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \Illuminate\Session\Middleware\StartSession::class
         ],
     ];
 
