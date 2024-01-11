b655abc6bab036f4e6d3fdd1da7335b658beeb7c
matondojoao <matondojoaokitemoco@gmail.com>
2024-01-11 12:51:34 +0100

    Inicia Transação com Stripe para Processamento de Pedido

--- app/Repositories/Customer/OrderRepository.php
+++ app/Repositories/Customer/OrderRepository.php
@@ -87,7 +87,7 @@ class OrderRepository
             $paymentIntent = \Stripe\PaymentIntent::create([
                 'amount' => $total * 100,
                 'currency' => 'brl',
-                'payment_method'=>'card',
+                'payment_method_types' =>['card','link'],
                 'automatic_payment_methods' => ['enabled' => true],
                 'customer' => $stripeCustomerId,
                 'description' => implode(', ', $itemDescriptions),
