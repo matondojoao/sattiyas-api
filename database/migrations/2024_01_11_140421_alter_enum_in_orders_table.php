78bfff77c84d53ad62b78ee303836064e1269f62
matondojoao <matondojoaokitemoco@gmail.com>
2024-01-11 15:18:32 +0100

    Inicia Transação com Stripe para Processamento de Pedido

--- database/migrations/2024_01_11_140421_alter_enum_in_orders_table.php
+++ database/migrations/2024_01_11_140421_alter_enum_in_orders_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('orders', function (Blueprint $table) {
-            $table->enum('status', ['processing', 'succeeded', 'failed'])->default('processing')->change();
+            $table->enum('payment_status', ['processing', 'succeeded', 'failed'])->default('processing')->change();
         });
     }
 
@@ -26,7 +26,7 @@ return new class extends Migration
     public function down()
     {
         Schema::table('orders', function (Blueprint $table) {
-            $table->enum('status', ['pending', 'processing', 'failed'])->change();
+            $table->enum('payment_status', ['pending', 'processing', 'failed'])->change();
         });
     }
 };
