874ff3f55e493bab816e7b083f8f2e79240d2f26
matondojoao <matondojoaokitemoco@gmail.com>
2024-01-11 16:56:23 +0100

    atualizacao metodo coupon

--- app/Repositories/Customer/OrderRepository.php
+++ app/Repositories/Customer/OrderRepository.php
@@ -18,10 +18,13 @@ class OrderRepository
         try {
             $cartItems = $data['cartItems'];
             $orderDetails = $data['orderDet'];
+            $coupon = $data['coupon'];
             $stripeToken = '';
 
             $cartItems = json_decode(json_encode($cartItems), true);
             $orderDetails = json_decode(json_encode($orderDetails), true);
+            $coupon = json_decode(json_encode($coupon), true);
+
 
             if (isset($orderDetails['_value'])) {
                 $orderDetails = $orderDetails['_value'];
@@ -79,8 +82,29 @@ class OrderRepository
                 return $item->price * $item->quantity;
             });
 
+            $totalDiscount = 0;
+
+
+            if (isset($coupon['_value'])) {
+                $coupon = $coupon['_value'];
+
+                if ($coupon['type'] == 'amount') {
+                    $totalDiscount = $coupon['value'];
+                } elseif ($coupon['type'] == 'percentage') {
+                    $totalDiscount -= ($totalDiscount * ($coupon['value'] / 100));
+                }
+
+                $couponData = \App\Models\Promotion::where('code', $coupon['code'])->first();
+
+                if ($couponData) {
+                    $couponData->increment('usage_count', 1);
+                }
+
+                $order->discount = $totalDiscount;
+                $order->save();
+            }
             if ($order->deliveryOption) {
-                $total += $order->deliveryOption->price;
+                $total += $order->deliveryOption->price - $totalDiscount;
             }
 
             $paymentIntent = Stripe\PaymentIntent::create([
@@ -107,140 +131,10 @@ class OrderRepository
             $order->update(['payment_status' => $paymentIntent->status]);
 
             return response()->json(['order_id' => $order->id], 200);
-
         } catch (\Illuminate\Database\QueryException $e) {
-            return response()->json(['error' => 'Erro ao salvar o pedido no banco de dados.', 'details' => $e->getMessage()], 500);
+            return response()->json(['error' => 'Error saving the order to the database.', 'details' => $e->getMessage()], 500);
         } catch (\Throwable $th) {
-            return response()->json(['error' => 'Erro desconhecido ao salvar o pedido.', 'details' => $th->getMessage()], 500);
-        }
-    }
-    public function placeOrder(array $data)
-    {
-        try {
-            Stripe\Stripe::setApiKey(env('STRIPE_SECRET'));
-
-            $customerEmail = $this->getAuthUser()->email;
-            $token = $data['stripeToken'];
-
-            $stripeCustomerId = $this->getStripeCustomerId($customerEmail, $token);
-
-            $cartItems = session()->get('cart', []);
-            $itemDescriptions = [];
-
-            $order = $this->getAuthUser()->orders()->create([
-                'delivery_option_id' => $data['delivery_option_id'],
-                'payment_status' => 'pending',
-                'fulfillment_status' => 'pending',
-                'payment_method_id' => $data['payment_method_id'],
-            ]);
-
-            $totalDiscount = 0;
-
-            if (session()->get('coupon')) {
-                $coupon = session()->get('coupon');
-
-                if ($order->deliveryOption) {
-                    $totalDiscount += $order->deliveryOption->price;
-                }
-
-                if ($coupon['type'] == 'amount') {
-                    $totalDiscount = $coupon['value'];
-                } elseif ($coupon['type'] == 'percentage') {
-                    $totalDiscount -= ($totalDiscount * ($coupon['value'] / 100));
-                }
-
-                $couponData = \App\Models\Promotion::where('code', $coupon['code'])->first();
-
-                if ($couponData) {
-                    $couponData->increment('usage_count', 1);
-                }
-
-                $order->discount = $totalDiscount;
-                $order->save();
-            }
-
-            $cartDetails = [];
-
-            foreach ($cartItems as $cartItem) {
-                $product = Product::find($cartItem['product_id']);
-
-                $product->stock()->decrement('quantity', $cartItem['quantity']);
-
-                if ($product) {
-
-                    $price = 0;
-                    if ($product->sale_price) {
-                        $price = $product->sale_price * $cartItem['quantity'];
-                    } else {
-                        $price = $product->regular_price * $cartItem['quantity'];
-                    }
-
-                    $cartDetails[] = [
-                        'product_id' => $cartItem['product_id'],
-                        'quantity' => $cartItem['quantity'],
-                        'price' => $price,
-                    ];
-                    $itemDescriptions[] = "{$product->name} ({$cartItem['quantity']}x)";
-                }
-            }
-
-            $order->orderItems()->createMany($cartDetails);
-
-            $total = 0;
-
-            foreach ($order->orderItems as $item) {
-                $total += $item->price * $item->quantity;
-            }
-
-            if ($order->deliveryOption) {
-                $total += $order->deliveryOption->price;
-            }
-
-            if (session()->has('coupon')) {
-                $coupon = session('coupon');
-
-                if ($coupon['type'] == 'amount') {
-                    $total -= $coupon['value'];
-                } elseif ($coupon['type'] == 'percentage') {
-                    $total -= ($total * ($coupon['value'] / 100));
-                }
-            }
-
-            $method = \Stripe\PaymentMethod::create([
-                'type' => 'card'
-              ]);
-
-            $paymentIntent = \Stripe\PaymentIntent::create([
-                'payment_method_types' => ['card'],
-                'payment_method' => $method->id,
-                'amount' => $total * 100,
-                'currency' => 'BRL',
-                'customer' => $stripeCustomerId,
-                'description' => implode(', ', $itemDescriptions),
-            ]);
-
-            $paymentIntentId = $paymentIntent->id;
-
-            $paymentIntent = \Stripe\PaymentIntent::retrieve($paymentIntentId);
-
-            $paymentIntent->confirm();
-
-            $pdf = PDF::loadView('order.invoice', ['order' => $order]);
-
-            $pdfPath = storage_path('app/public/orders/order_' . $order->id . '.pdf');
-            $pdf->save($pdfPath);
-
-            $order->user->notify(new OrderPlacedNotification($pdfPath, $order));
-
-            session()->forget('cart', []);
-
-            session()->forget('coupon');
-
-            return response()->json(['client_secret' => $paymentIntent->client_secret], 200);
-        } catch (\Stripe\Exception\CardException $e) {
-            return response()->json(['message' => 'Card error. Please check your card details and try again.'], 400);
-        } catch (\Exception $e) {
-            return response()->json(['message' => 'An unexpected error occurred. Please try again later.' . $e], 500);
+            return response()->json(['error' => 'Unknown error while saving the order.', 'details' => $th->getMessage()], 500);
         }
     }
 
@@ -287,11 +181,11 @@ class OrderRepository
 
     public function find(string $id)
     {
-        return $this->getAuthUser()->orders()->with('orderItems.product','deliveryOption')->findOrFail($id);
+        return $this->getAuthUser()->orders()->with('orderItems.product', 'deliveryOption')->findOrFail($id);
     }
 
     public function getUserOrders()
     {
-        return $this->getAuthUser()->orders()->with('orderItems.product','deliveryOption')->orderBy('created_at', 'desc')->paginate(10);
+        return $this->getAuthUser()->orders()->with('orderItems.product', 'deliveryOption')->orderBy('created_at', 'desc')->paginate(10);
     }
 }
